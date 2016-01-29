be14ca1b182d615d5ce4c4f9d69acb91eeabd953
Zhong Zhuang <naituida@163.com>
2016-01-29 23:21:17 +0800

    testing http hander for bitcoind

---
+++ crypto/hash.go
@@ -0,0 +1,37 @@
+package crypto
+
+import (
+	"crypto/sha256"
+	"github.com/dyzz/gobtclib/crypto/ripemd160"
+)
+
+func ShaHash(b []byte, out []byte) {
+	s := sha256.New()
+	s.Write(b[:])
+	tmp := s.Sum(nil)
+	s.Reset()
+	s.Write(tmp)
+	copy(out[:], s.Sum(nil))
+}
+
+// Returns hash: SHA256( SHA256( data ) )
+// Where possible, using ShaHash() should be a bit faster
+func Sha2Sum(b []byte) (out [32]byte) {
+	ShaHash(b, out[:])
+	return
+}
+
+func RimpHash(in []byte, out []byte) {
+	sha := sha256.New()
+	sha.Write(in)
+	rim := ripemd160.New()
+	rim.Write(sha.Sum(nil)[:])
+	copy(out, rim.Sum(nil))
+}
+
+// Returns hash: RIMP160( SHA256( data ) )
+// Where possible, using RimpHash() should be a bit faster
+func Rimp160AfterSha256(b []byte) (out [20]byte) {
+	RimpHash(b, out[:])
+	return
+}
--- main.go
+++ main.go
@@ -1,30 +1,84 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/btcsuite/btcrpcclient"
 	"github.com/davecgh/go-spew/spew"
-	"log"
+	"github.com/libreoscar/utils/log"
+	"io"
+	"net/http"
+	"os"
 )
 
-func main() {
-	connCfg := &btcrpcclient.ConnConfig{
-		Host:         "localhost:18332",
-		User:         "braft",
-		Pass:         "braft",
-		HTTPPostMode: true,
-		DisableTLS:   true,
-	}
+var client *btcrpcclient.Client
+var logger = log.New(log.DEBUG)
 
-	client, err := btcrpcclient.New(connCfg, nil)
+func loadConf() *btcrpcclient.ConnConfig {
+	file, _ := os.Open("conf.json")
+	decoder := json.NewDecoder(file)
+	rpcConf := &btcrpcclient.ConnConfig{}
+	err := decoder.Decode(rpcConf)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("error:", err)
+	}
+	rpcConf.HTTPPostMode = true
+	rpcConf.DisableTLS = true
+	return rpcConf
+}
+
+func checkBlock(client *btcrpcclient.Client, blockNum int64) {
+	blockHash, err := client.GetBlockHash(blockNum)
+	if err != nil {
+		logger.Crit(err.Error())
+		return
+	}
+	block, err := client.GetBlock(blockHash)
+	if err != nil {
+		logger.Crit(err.Error())
+		return
+	}
+	for _, tx := range block.Transactions() {
+		fmt.Printf("Tx ID: %v\n", tx.Sha())
+		for _, vout := range tx.MsgTx().TxOut {
+			addr := NewAddrFromPkScript(vout.PkScript, true)
+			spew.Dump(addr)
+		}
+		fmt.Println("===========")
+	}
+}
+
+func blockNotify(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Received new block!")
+	blockNum, err := client.GetBlockCount()
+	if err != nil {
+		io.WriteString(w, "bitcoind rpc failed\n")
+	}
+	checkBlock(client, blockNum)
+}
+
+func main() {
+	connCfg := loadConf()
+	var err error
+
+	client, err = btcrpcclient.New(connCfg, nil)
+	if err != nil {
+		logger.Crit(err.Error())
+		return
 	}
 	defer client.Shutdown()
 
-	// Get the current block count.
-	info, err := client.GetInfo()
+	// // Get the current block count.
+	// info, err := client.GetInfo()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Printf("Bitcoind Info: %v", spew.Sdump(info))
+
+	http.HandleFunc("/block", blockNotify)
+	logger.Info("Starting server...")
+	err = http.ListenAndServe(":8000", nil)
 	if err != nil {
-		log.Fatal(err)
+		logger.Crit(err.Error())
 	}
-	log.Printf("Bitcoind Info: %v", spew.Sdump(info))
 }
